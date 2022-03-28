ab79e6cc5ecafec6516c433b1039dd7f1d27c719
Angel Eduardo Ramos Mena <angedramos@gmail.com>
2022-03-28 11:08:09 -0500

    Adding CSRF Protection

---
+++ src/main/java/com/mvcblogs/blog/WebMVCSecurity.java
@@ -0,0 +1,19 @@
+package com.mvcblogs.blog;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class WebMVCSecurity extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+    }
+}
